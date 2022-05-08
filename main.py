51fbea2429f54c5c4c1b68c5b07bd2027bda3a39
Kavin Bharathi <66783110+cipherDOT@users.noreply.github.com>
2022-05-08 11:58:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# learning AI by making a game and using genetic algorithm (NEAT)...
-# NEAT = NeuroEvolution of Augmented Topologies
 
 
 import pygame
@@ -13,10 +11,10 @@ D_HEIGHT = 800
 GEN = 0
 
 display = pygame.display.set_mode((D_WIDTH, D_HEIGHT))
-BIRD_IMG =  pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird2.png')))
-PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'pipe.png')))
-BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'base.png')))
-BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bg.png')))
+BIRD_IMG =  pygame.transform.scale2x(pygame.image.load(os.path.join('assets', 'bird2.png')))
+PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join('assets', 'pipe.png')))
+BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join('assets', 'base.png')))
+BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join('assets', 'bg.png')))
 pygame.display.set_caption('Flappy bird')
 pygame.display.set_icon(BIRD_IMG)
 

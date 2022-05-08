d7518e922f32367747498f7245ca7ce5eca7837a
Kavin Bharathi <66783110+cipherDOT@users.noreply.github.com>
2022-05-08 12:05:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ pygame.font.init()
 
 D_WIDTH = 500
 D_HEIGHT = 800
-GEN = 0
 
 display = pygame.display.set_mode((D_WIDTH, D_HEIGHT))
 BIRD_IMG =  pygame.transform.scale2x(pygame.image.load(os.path.join('assets', 'bird.png')))
@@ -16,12 +15,15 @@ BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join('assets', 'bg.p
 pygame.display.set_caption('Flappy bird')
 pygame.display.set_icon(BIRD_IMG)
 
+# -------------------------------------------------------------------------------------------------------------------------#
+
 def blitRotateCenter(surf, image, topleft, angle):
     rotated_image = pygame.transform.rotate(image, angle)
     new_rect = rotated_image.get_rect(center = image.get_rect(topleft = topleft).center)
     surf.blit(rotated_image, new_rect)
 
 # -------------------------------------------------------------------------------------------------------------------------#
+
 class Bird:
     def __init__(self, x, y, vel, img):
         self.x = x
